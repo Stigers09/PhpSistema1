791639afeb62b5ab47ba29ae0ba06376470ad247
Jonathan Rodriguez <50118648+Jonathanl55@users.noreply.github.com>
2021-04-23 16:34:51 -0500

    Update footer_admin.php (#26)
    * Eliminar usuarios.php
    
    * Create Usuarios.php
    
    * Create UsuariosModel.php
    
    * Create functions_usuarios.js
    
    * Update footer_admin.php

--- Vistas/Template/footer_admin.php
+++ Vistas/Template/footer_admin.php
@@ -1,25 +1,31 @@
 <script>
   const base_url = "<?= base_url(); ?>";
-  </script>
+</script>
 <!-- Essential javascripts for application to work-->
     <script src="<?=mediaStivenjs ();?>/jquery-3.3.1.min.js"></script>
     <script src="<?=mediaStivenjs ();?>/popper.min.js"></script>
     <script src="<?=mediaStivenjs ();?>/bootstrap.min.js"></script>
     <script src="<?=mediaStivenjs ();?>/main.js"></script>
-  
     <script src="<?=mediaStivenjs ();?>/functions_admin.js"></script>
+
     <!-- The javascript plugin to display page loading on top-->
     <script src="<?=mediaStivenjs ();?>/plugins/pace.min.js"></script>
+
     <!-- Page specific javascripts-->
     <script type="text/javascript" src="<?=mediaStivenjs ();?>/plugins/sweetalert.min.js"></script>
-     <!-- Data table plugin-->
 
+     <!-- Data table plugin-->
     <script type="text/javascript" src="<?=mediaStivenjs(); ?>/plugins/jquery.dataTables.min.js"></script>
     <script type="text/javascript" src="<?=mediaStivenjs(); ?>/plugins/dataTables.bootstrap.min.js"></script>
-    
-    <script src="<?=mediaStivenjs(); ?>/functions_roles.js"></script>
+
     <script src="<?=mediaStivenjs(); ?>/js/<?= $data['page_functions_js']; ?>"></script>
     
+    <?php if($data['page_name'] == "rol_usuario"){ ?>
+    <script src="<?=mediaStivenjs(); ?>/functions_roles.js"></script>
+    <?php } ?>
+    <?php if($data['page_name'] == "usuario"){ ?>
+    <script src="<?=mediaStivenjs(); ?>/functions_usuarios.js"></script>
+    <?php } ?>
 
   </body>
 </html>

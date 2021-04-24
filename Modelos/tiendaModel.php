31f001fa179a2cc864857818c2c5204bb451e288
Edwin <Edwin@Pc-Edwin>
2021-04-23 19:13:53 -0500

    imagenes tienda desde bd productos

--- Modelos/tiendaModel.php
+++ Modelos/tiendaModel.php
@@ -11,6 +11,10 @@
         {
             parent::__construct(); // Ejecuta constructor padre (mysql)
         }
-        
+        public function selectProductos2(){
+            $sql = "SELECT * FROM bdnubecitas.productos";
+            $request = $this->select_all2($sql);
+            return $request;
+        }
     }
 ?>
--- Vistas/Tienda/tienda.php
+++ Vistas/Tienda/tienda.php
@@ -1,3 +1,24 @@
 <?php headerTienda($data); ?>
-<?php sectionTienda($data); ?>
+<section>        
+    <div class="h2"><h2><?= $data['page_title']; ?></h2></div>
+    
+    <?php 
+    $obj = new Tienda();
+        $data =$obj->getProductos2();
+        // dep($data);
+        
+        foreach ($data as $k) { ?>
+    <div class="images">
+        <div class="cont">
+        <a class="section__a1" href=""><img src="data:image/png;base64,<?php echo base64_encode($k->imagen); ?>"></a>	
+            <ul>                
+                <li><?php echo $k->nombre; ?></li>
+                <li><?php echo "$".formatMoney($k->precio); ?></li>
+            </ul>							
+        </div>
+        <?php 
+        }
+        ?>
+    </div>			
+</section>
 <?php footerTienda($data); ?>    

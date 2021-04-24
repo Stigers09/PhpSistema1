bc77a6c6dd9a19e9903c0803f1d9583be5f6525b
Stigers09 <81544338+Stigers09@users.noreply.github.com>
2021-04-23 21:47:19 -0500

    LOGIN FUNCIONAL (#32)
    * Create Stiven.html.txt
    
    Este agregue a las 3 y 55
    
    * Create stiven.html.txt
    
    * coomit
    
    * commit
    
    * master
    
    * Create Stiger.html
    
    * commit
    
    * master
    
    * Create Stiven09.html
    
    * master
    
    * commit
    
    * 'master'
    
    * commit
    
    * commit
    
    * master
    
    * master
    
    * master
    
    * master
    
    * Revert "imagenes tienda desde bd productos"
    
    This reverts commit 31f001fa179a2cc864857818c2c5204bb451e288.
    
    * master
    
    * master
    
    Co-authored-by: EdwinSalamandro <80175396+EdwinSalamandro@users.noreply.github.com>

--- Vistas/Template/footer_admin.php
+++ Vistas/Template/footer_admin.php
@@ -1,31 +1,37 @@
 <script>
   const base_url = "<?= base_url(); ?>";
-</script>
+  </script>
 <!-- Essential javascripts for application to work-->
     <script src="<?=mediaStivenjs ();?>/jquery-3.3.1.min.js"></script>
     <script src="<?=mediaStivenjs ();?>/popper.min.js"></script>
     <script src="<?=mediaStivenjs ();?>/bootstrap.min.js"></script>
     <script src="<?=mediaStivenjs ();?>/main.js"></script>
+  
     <script src="<?=mediaStivenjs ();?>/functions_admin.js"></script>
-
     <!-- The javascript plugin to display page loading on top-->
     <script src="<?=mediaStivenjs ();?>/plugins/pace.min.js"></script>
-
     <!-- Page specific javascripts-->
     <script type="text/javascript" src="<?=mediaStivenjs ();?>/plugins/sweetalert.min.js"></script>
-
      <!-- Data table plugin-->
+
     <script type="text/javascript" src="<?=mediaStivenjs(); ?>/plugins/jquery.dataTables.min.js"></script>
     <script type="text/javascript" src="<?=mediaStivenjs(); ?>/plugins/dataTables.bootstrap.min.js"></script>
-
-    <script src="<?=mediaStivenjs(); ?>/js/<?= $data['page_functions_js']; ?>"></script>
     
-    <?php if($data['page_name'] == "rol_usuario"){ ?>
+
+    <script src="<?=mediaStivenjs(); ?>/functions_admin.js"></script>
+    <script src="<?=mediaStivenjs(); ?>/<?= $data['page_functions_js']; ?>"></script>
+    
+
+    <?php
+    if($data['page_name'] == "roles"){ ?>
     <script src="<?=mediaStivenjs(); ?>/functions_roles.js"></script>
+    <script src="<?=mediaStivenjs(); ?>/js/<?= $data['page_functions_js']; ?>"></script>
     <?php } ?>
-    <?php if($data['page_name'] == "usuario"){ ?>
-    <script src="<?=mediaStivenjs(); ?>/functions_usuarios.js"></script>
+    <?php if($data['page_name'] == "usuarios"){ ?>
+    <script src="<?=mediaStivenjs(); ?>/js/functions_usuarios.js"></script>
+    <script src="<?=mediaStivenjs(); ?>/js/<?= $data['page_functions_js']; ?>"></script>
     <?php } ?>
+
 
   </body>
 </html>

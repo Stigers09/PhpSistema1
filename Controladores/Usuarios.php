8c45cdd37359bf721f11933378e6a60fce964d2a
Stiven Sanchez <stiven09192000@gmail.com>
2021-04-23 21:26:09 -0500

    master

--- Controladores/Usuarios.php
+++ Controladores/Usuarios.php
@@ -6,6 +6,12 @@ class Usuarios extends Controllers
     public function __construct()
     {
         parent::__construct();
+        
+        session_start();
+        if(empty($_SESSION['login']))
+        {
+            header('Location: '.base_url().'login');
+        }
     }
 
     public function usuarios()
